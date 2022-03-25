50b1ba312ca15d626860d2516f03307a949693b1
Ethan Dunn <Ethan05Dunn@gmail.com>
2022-03-25 15:06:03 -0500

    Fixed gitignore to add the Artifact folder

---
+++ ArtifactOfDrift/Artifacts/Base/Artifact.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using ArtifactOfDrift.Util;
+using R2API;
+using R2API.Utils;
+using RoR2;
+using UnityEngine;
+using Path = System.IO.Path;
+
+namespace ArtifactOfDrift.Artifacts.Base;
+
+[R2APISubmoduleDependency(nameof(ContentAddition))]
+public class Artifact
+{
+    public ArtifactDef Def = ScriptableObject.CreateInstance<ArtifactDef>();
+
+    public Artifact(string name, string description)
+    {
+        BasePath = "Artifacts" + "/" + name;
+        
+        Def.nameToken = name;
+        Def.descriptionToken = description;
+        
+        Def.smallIconSelectedSprite = Assets.LoadAsset<Sprite>($"{BasePath}/enabled.png") 
+                                      ?? throw new FileNotFoundException();
+        Def.smallIconDeselectedSprite = Assets.LoadAsset<Sprite>($"{BasePath}/disabled.png") 
+                                        ?? throw new FileNotFoundException();
+        
+        ContentAddition.AddArtifactDef(Def);
+    }
+    
+    public string BasePath { get; }
+}
---
+++ ArtifactOfDrift/Artifacts/DriftArtifact.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using ArtifactOfDrift.Artifacts.Base;
+using ArtifactOfDrift.Util;
+using RoR2;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.Networking;
+using Run = On.RoR2.Run;
+using SceneDirector = On.RoR2.SceneDirector;
+
+namespace ArtifactOfDrift.Artifacts;
+
+public static class DriftArtifact
+{
+    private static Artifact Drift;
+
+    private const string Name = "Artifact of Drift";
+    private const string Description = "Every stage is randomized.";
+    private const string LunarTeleporterKey =
+        "RoR2/Base/Teleporters/iscLunarTeleporter.asset";
+    
+    private static bool Enabled => RunArtifactManager.instance.IsArtifactEnabled(Drift.Def.artifactIndex);
+
+    public static void Initialize()
+    {
+        Drift = new Artifact(Name, Description);
+
+        Hooks();
+    }
+    
+    
+    private static void Hooks()
+    {
+        Run.PickNextStageScene += RunOnPickNextStageScene;
+        SceneDirector.PlaceTeleporter += SceneDirectorOnPlaceTeleporter;
+    }
+
+    private static void SceneDirectorOnPlaceTeleporter(SceneDirector.orig_PlaceTeleporter orig, RoR2.SceneDirector self)
+    {
+        if (Enabled && (RoR2.Run.instance.stageClearCount + 1) % 5 == 0)
+        {
+            var pt = Addressables
+                .LoadAssetAsync<InteractableSpawnCard>(LunarTeleporterKey).WaitForCompletion();
+            self.teleporterSpawnCard = pt;
+        }
+        
+        orig(self);
+    }
+
+    private static void RunOnPickNextStageScene(Run.orig_PickNextStageScene orig, RoR2.Run self, WeightedSelection<SceneDef> choices)
+    {
+        var validStages = SceneCatalog.allStageSceneDefs
+            .Where(stage => stage.validForRandomSelection)
+            .Where(stage => stage.requiredExpansion == null || self.IsExpansionEnabled(stage.requiredExpansion))
+            .ToArray();
+
+        if (Enabled)
+        {
+            choices = new WeightedSelection<SceneDef>();
+            foreach (var stage in validStages)
+            {
+                choices.AddChoice(stage, 1f);
+            }
+        }
+
+        orig(self, choices);
+    }
+}
